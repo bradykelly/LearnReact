1796054049e4478e5d2e2d12bb1347b53d2d53f6
Brady Kelly <brady.kelly@xisystems.co.za>
2022-07-23 19:32:05 +0200

    Up until website link

--- BusinessCard/src/Card.jsx
+++ BusinessCard/src/Card.jsx
@@ -1,11 +1,16 @@
-﻿
-
-const Card = () => {
+﻿const Card = () => {
     return (
         <div className="card">
-            Hello world!
-            <div className="card-top">
-                Hello Image
+            <div className="card-image"></div>
+            <div className="card-content text">
+                <div className="center">
+                    <h1>Laura Smith</h1>
+                    <h3>Frontend Developer</h3>
+                    <a href="#">Laura's Website</a>
+                </div>
+                <div className="buttons">
+                    
+                </div> 
             </div>
         </div>
     );
