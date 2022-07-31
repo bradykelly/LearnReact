509427f47ad076134309cfdcfb944a567bd3cab7
Brady Kelly <brady.kelly@xisystems.co.za>
2022-07-31 06:40:03 +0200

    Navbar and Hero looking OK

---
+++ AirBnb/src/Hero.jsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import Group from "./assets/hero-group.png"
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-images">
+                <img src={ Group } alt="airbnb photo group" />
+            </div>
+            <div className="hero-text">
+                <h1>Online experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
---
+++ AirBnb/src/NavBar.jsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import Logo from "./assets/airbnb-logo.png";
+
+const NavBar = () => {
+    return (
+      <nav>
+          <img src={Logo} alt="airbnb logo" />
+      </nav>
+    );
+}
+
+export default NavBar;
