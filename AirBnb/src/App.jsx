94ae4ab0f0f859bb2bbc55aadcbeb466e3984cee
Brady Kelly <brady.kelly@xisystems.co.za>
2022-07-31 17:36:13 +0200

    Card hard-code completed

--- AirBnb/src/App.jsx
+++ AirBnb/src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
-import './App.css'
 import Hero from "./Hero.jsx";
+import Card from "./Card.jsx";
+import './App.css'
 
 const App = () => {
-
     return (
-        <div className="container">
-            <NavBar />
-            <Hero />
-        </div>
+        <Card />
     )
 }
 
---
+++ AirBnb/src/Card.jsx
@@ -0,0 +1,17 @@
+﻿import React from "react";
+import katie from "./assets/katie-zaferes.png";
+
+const Card = (props) => {
+    return (
+        <div className="card">
+            <div className="card-image"></div>
+            <div className="card-text">
+                <p className="rating"><strong>5.0</strong> (6) - USA</p>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><strong>From $136</strong> / person</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
