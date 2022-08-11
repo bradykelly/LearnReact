71290085012e2a406347514266248c0eb7d3ca91
Brady Kelly <brady.kelly@xisystems.co.za>
2022-08-11 15:46:42 +0200

    New TodoistClient.Cli

---
+++ LearnReact.TodoistClient.Cli/Program.cs
@@ -0,0 +1,7 @@
+using LearnReact.TodoistClient.Cli;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
