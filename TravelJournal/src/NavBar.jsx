2b7d99b61bc1c205f94d5b5fb344631a8afbdec8
Brady Kelly <brady.kelly@xisystems.co.za>
2022-08-28 05:42:41 +0200

    Centred navbar title

---
+++ TravelJournal/src/NavBar.jsx
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import styles from './styles/NavBar.module.css';
+import world from "./assets/world.png";
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className={styles["title"]}>
+                <img src={world} alt="world icon" />
+                <span>my travel journal</span>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
